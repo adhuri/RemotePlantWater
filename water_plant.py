f771179951ee9b6352cd3170e126f88fbacdaef3
Aniket D <aniket.dhuri@gmail.com>
2020-06-22 23:02:16 -0700

    Changed the config level and format

--- water_plant.py
+++ water_plant.py
@@ -1,6 +1,8 @@
 from devices.motor import Motor
 import logging
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-4s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d-%Y %H:%M:%S')
 
 if __name__ == "__main__":
     m = Motor(name = "Motor Indoor")
