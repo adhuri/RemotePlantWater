03cd3b0d3943a053f84ac869f5d2dff9a99e87c5
Aniket D <aniket.dhuri@gmail.com>
2020-06-23 23:39:42 -0700

    Added motor to run for multiple cycles

--- devices/motor.py
+++ devices/motor.py
@@ -1,6 +1,6 @@
 from devices.device import Device
 import time
-
+from typing import Dict
 import logging 
 logger = logging.getLogger("water_plants")
 
@@ -15,7 +15,27 @@ class Motor(Device):
         logger.debug(f"Running for {pump_duration} seconds")
         self.turnONGPIO()
         time.sleep(pump_duration)
+
              
     def stop(self):
         logger.info(f"Stopping Motor: \"{self.name}\"")   
         self.turnOFFGPIO()
+
+    def run(self):       
+        cycles = self.no_of_cycles()
+        logger.info(f"Running the pump for cycles : {cycles}")
+        for cycle in range(1,cycles+1):
+            logger.info(f"Cycle No: {cycle}")
+            self.start()
+            self.stop()
+            logger.debug(f"Waiting for duration in seconds: {self.duration_between_cycles()}")
+            time.sleep(self.duration_between_cycles())
+
+    def get_schedule(self) -> [str,str,str]:
+        return self.readConfig("ScheduledDateTime").split(":")
+
+    def no_of_cycles(self) -> int:
+        return int(self.readConfig("Cycles"))
+
+    def duration_between_cycles(self) -> int:
+        return int(self.readConfig("DurationBetweenCycles"))
